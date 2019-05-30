82bbf8198ae842f7452ba922141b610d2ecfafe0
JasperHale <ljy087621@gmail.com>
2019-05-30 20:55:22 +0800

    dfs.path

--- data_structure/Graph/DFS.py
+++ data_structure/Graph/DFS.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-from GH import Graph
+from GH import Graph, Search, Paths
 from gif import getGr, gif, dot_node
 
 
-class DFS(object):
+class DFS(Search):
     marked: list = []
     count: int = 0
 
@@ -25,6 +25,42 @@ class DFS(object):
 
     def rcount(self) -> int:
         return self.count
+
+
+class DFP(Paths):
+    marked: list[bool] = []
+    edgeTo: list[int] = []
+    s: int = 0
+
+    def __init__(self, gr: Graph, s: int):
+        self.marked = [False] * gr.rV()
+        self.marked = [0] * gr.rV()
+        self.s = s
+        self.dfs(gr, s)
+
+    def dfs(self, gr: Graph, v: int):
+        self.marked[v] = True
+        for w in gr.adjlist(v):
+            if not self.rmarked(w):
+                self.edgeTo[w] = v
+                self.dfs(gr, w)
+
+    def hasPathTo(self, v: int) -> bool:
+        return self.marked[v]
+
+    def pathTo(self, v: int) -> list:
+        if not self.hasPathTo(v):
+            return None
+
+        path = []
+        path.append(v)
+        x = self.edgeTo[v]
+
+        while x != v:
+            x = self.edgeTo[x]
+            path.append(x)
+        path.append(self.s)
+        return path
 
 
 if __name__ == "__main__":
--- data_structure/Graph/GH/__init__.py
+++ data_structure/Graph/GH/__init__.py
@@ -3,6 +3,7 @@
 from .GHclass import GH
 from .Graph import Graph, Graph_AM
 from .Digraph import Digraph, Digraph_AM
+from .SearchClass import Search, Paths
 import sys
 sys.path.append('/home/jasperhale/software/work/python')
 from libs import prgraph, randoms
